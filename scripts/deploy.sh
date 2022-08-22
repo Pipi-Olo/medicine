3605c56c39e17dd760308e042d93bc0cfecd643f
pipiolo <pipiolo1024@gmail.com>
2022-08-22 19:02:11 +0900

    Add deploy shell script from jenkins to ec2

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/deploy
+
+CURRENT_PID=$(pgrep -f $REPOSITORY/*.jar)
+echo "현재 구동 중인 Application Pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 Application 이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
+  sleep 5
+fi
+
+echo "> New Application 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+echo "> JAR NAME : $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x "$JAR_NAME"
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $REPOSITORY/*.jar 2>&1 &
