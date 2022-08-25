440553da39e0ef9efe45c3626fdfd72bff01a088
pipiolo <pipiolo1024@gmail.com>
2022-08-25 18:01:04 +0900

    JAR 파일 위치 변경 및 sudo 명령어 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,14 +8,14 @@ echo "현재 구동 중인 Application Pid : $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 Application 이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 "$CURRENT_PID"
+  echo "> sudo kill -15 $CURRENT_PID"
+  sudo kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
 echo "> New Application 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar | tail -n 1)
 echo "> JAR NAME : $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
