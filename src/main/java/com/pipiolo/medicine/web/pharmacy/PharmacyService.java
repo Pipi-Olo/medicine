79eafc8f24075fc415f3cd0bd33cacec265ca1ce
pipiolo <pipiolo1024@gmail.com>
2022-08-30 00:57:35 +0900

    AWS RDS 한국어 업데이트 및 테스트 데이터 제거

--- src/main/java/com/pipiolo/medicine/web/pharmacy/PharmacyService.java
+++ src/main/java/com/pipiolo/medicine/web/pharmacy/PharmacyService.java
@@ -1,6 +1,5 @@
 package com.pipiolo.medicine.web.pharmacy;
 
-import com.pipiolo.medicine.domain.pharmacy.Pharmacy;
 import com.pipiolo.medicine.domain.pharmacy.PharmacyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +16,6 @@ public class PharmacyService {
             PharmacySearchCondition searchCondition,
             Pageable pageable
     ) {
-        pharmacyRepository.save(new Pharmacy("별약국", "031-757-8426", "경기도 성남시 수정구 청계산로 689, 1층 103호 (고등동)"));
-
         return pharmacyRepository.findSliceBySearchCondition(searchCondition, pageable)
                 .map(PharmacyResponse::new);
     }
